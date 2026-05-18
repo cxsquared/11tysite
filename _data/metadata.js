994a60688eab7f03acf6f51400c33b486523490f
cxsquared <cxsquared@gmail.com>
2026-05-18 11:46:59 -0400

    Updated updates

--- _data/metadata.js
+++ _data/metadata.js
@@ -13,6 +13,7 @@ export default {
     shortname: "codyclaborn",
   },
   updates: [
+    "5/18 - Basic photo album done",
     "5/16 - Cooler 404, better footer locaiton, and some photo album progress",
     "5/13 - Changing up sidebar and new post",
     "1/20 - New games.",
